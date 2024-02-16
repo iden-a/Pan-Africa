7ca477ff553d56eac368727182ed65c2d8be0033
iden-a <idenamoako@gmail.com>
2024-02-16 18:38:03 -0500

    updating media queries and hover colors

--- project/src/Reviews.jsx
+++ project/src/Reviews.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import './Reviews.css';
 
 export default function Reviews() {
   const { alias } = useParams();
